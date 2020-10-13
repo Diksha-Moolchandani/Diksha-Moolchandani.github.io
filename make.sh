9e8ba3f39f81322d317cae795b269d8b13edc8a7
Diksha-Moolchandani <diksha.moolchandani@cse.iitd.ac.in>
2020-10-13 19:43:19 +0530

    changes

--- make.sh
+++ make.sh
@@ -1,1 +1,1 @@
-python2 jemdoc.py index.jemdoc talks.jemdoc internship.jemdoc cv.jemdoc projects.jemdoc honors.jemdoc software.jemdoc bio.jemdoc
+python2 jemdoc.py index.jemdoc talks.jemdoc internship.jemdoc cv.jemdoc projects.jemdoc honors.jemdoc software.jemdoc bio.jemdoc ta.jemdoc
